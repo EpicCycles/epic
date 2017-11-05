0d4e7cb27d710e5b6ac853868427aff69b8a828e
Anna Epic <appdev.epiccycles@gmail.com>
2017-11-05 17:38:00 +0000

    Changes to QUote page for fitting and generic Notes element

--- epic/view_helpers/note_view_helper.py
+++ epic/view_helpers/note_view_helper.py
@@ -5,13 +5,21 @@ from epic.models import CustomerNote
 
 
 def create_customer_note(request, customer, quote, customer_order):
-    note_type = request.POST.get('note_type', '')
-    note_contents = request.POST.get('note_contents', '')
+    note_contents_epic = request.POST.get('note_contents_epic', '')
+    note_contents_cust = request.POST.get('note_contents_cust', '')
 
-    if note_contents != '':
-        note_text = note_contents
+    if note_contents_epic != '':
+        note_text = note_contents_epic
         created_by = request.user
-        customer_visible = (note_type == "customer")
+        customer_visible = False
+
+        customerNote = CustomerNote(customer=customer, quote=quote, customerOrder=customer_order, note_text=note_text,
+                                    created_by=created_by, customer_visible=customer_visible)
+        customerNote.save()
+    if note_contents_cust != '':
+        note_text = note_contents_cust
+        created_by = request.user
+        customer_visible = True
 
         customerNote = CustomerNote(customer=customer, quote=quote, customerOrder=customer_order, note_text=note_text,
                                     created_by=created_by, customer_visible=customer_visible)
