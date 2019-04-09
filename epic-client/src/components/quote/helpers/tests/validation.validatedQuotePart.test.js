1e6891a6805612218e8be8ae7c9bd197994977c3
Anna <appdev.epiccycles@gmail.com>
2019-04-09 13:49:32 +0100

    QUote part validation and tests

---
+++ epic-client/src/components/quote/helpers/tests/validation.validatedQuotePart.test.js
@@ -0,0 +1,145 @@
+import {quotePartValidation} from "../validation";
+
+describe('validatedQuotePart', () => {
+    const normalPartType = { id: 16, can_be_substituted: true, can_be_omitted: true };
+    const requiredPartType = { id: 16, can_be_substituted: true, can_be_omitted: false };
+    const noAlternatePartType = { id: 16, can_be_substituted: false, can_be_omitted: true };
+    const parts = [
+        { id: 12, part_name: 'bike', brand: 1, partType: 16 },
+        { id: 13, part_name: 'other', brand: 2, partType: 16 },
+    ];
+    const brands = [
+        { id: 1, brand_name: 'B1' },
+        { id: 2, brand_name: 'B2' },
+    ];
+    it('should return no errors when all fields are OK', () => {
+        const startPart = {
+            part_desc: 'B1 Bike',
+            quantity: 1,
+            quote_price: 12.99,
+        };
+        const validatedPart = {
+            part_desc: 'B1 Bike',
+            part: { id: 12, part_name: 'bike', brand: 1, partType: 16 },
+            quantity: 1,
+            quote_price: 12.99,
+            error: false,
+            error_detail: {}
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+    it('should return no errors when all fields are OK for a replacement part', () => {
+        const startPart = {
+            part_desc: 'B2 other',
+            not_required: true,
+            quote_price: 12.99,
+        };
+        const validatedPart = {
+            part_desc: 'B2 other',
+            not_required: true,
+            quantity: '',
+            part: { id: 13, part_name: 'other', brand: 2, partType: 16 },
+            quote_price: 12.99,
+            error: false,
+            error_detail: {}
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+    it('should return no errors when all fields are OK for an omitted part', () => {
+        const startPart = {
+            not_required: true,
+            quote_price: 12.99,
+        };
+        const validatedPart = {
+            not_required: true,
+            part: undefined,
+            quantity: '',
+            quote_price: 12.99,
+            additional_data: '',
+            error: false,
+            error_detail: {}
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+    it('should return part error when part is not found', () => {
+        const startPart = {
+            part_desc: 'B3 Bike',
+            quantity: 1,
+            quote_price: 12.99,
+        };
+        const validatedPart = {
+            part_desc: 'B3 Bike',
+            part: undefined,
+            quantity: '',
+            quote_price: 12.99,
+            additional_data: '',
+            error: true,
+            error_detail: { part_desc: 'Please include a brand in the part name to add this part.' }
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+    it('should return no price error when new part with no price', () => {
+        const startPart = {
+            part_desc: 'B1 Bike',
+            quantity: 1,
+        };
+        const validatedPart = {
+            part_desc: 'B1 Bike',
+            part: { id: 12, part_name: 'bike', brand: 1, partType: 16 },
+            quantity: 1,
+            error: true,
+            error_detail: {
+                quote_price: 'Please specify a price (can be negative).',
+            }
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+    it('should return no errors when price errors when price not provided for a replacement part', () => {
+        const startPart = {
+            part_desc: 'B2 other',
+            not_required: true,
+        };
+        const validatedPart = {
+            part_desc: 'B2 other',
+            not_required: true,
+            quantity: '',
+            part: { id: 13, part_name: 'other', brand: 2, partType: 16 },
+            error: true,
+            error_detail: {
+                quote_price: 'Please specify a price (can be negative).',
+            }
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+    it('should return price errors when price not provided for an omitted part', () => {
+        const startPart = {
+            not_required: true,
+        };
+        const validatedPart = {
+            not_required: true,
+            part: undefined,
+            quantity: '',
+            additional_data: '',
+            error: true,
+            error_detail: {
+                quote_price: 'Please specify a price (can be negative).',
+            }
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+    });
+it('should return a quantity error when this is not a replacement part and there is no quantity', () => {
+    const startPart = {
+            part_desc: 'B1 Bike',
+            quote_price: 12.99,
+        };
+        const validatedPart = {
+            part_desc: 'B1 Bike',
+            part: { id: 12, part_name: 'bike', brand: 1, partType: 16 },
+            quote_price: 12.99,
+            error: true,
+            error_detail: {quantity: 'Quantity is required for non replacement parts.'}
+        };
+        expect(quotePartValidation(startPart, undefined, normalPartType, brands, parts)).toEqual(validatedPart);
+
+})
+});
--- epic-client/src/components/quote/helpers/validation.js
+++ epic-client/src/components/quote/helpers/validation.js
@@ -1,5 +1,5 @@
-import {updateObject} from "../../../helpers/utils";
-import {hasErrors} from "../../app/model/helpers/model";
+import {isItAnObject, updateObject} from "../../../helpers/utils";
+import {findPartWithDescription} from "../../part/helpers/part";
 
 const BIKE_OR_PARTS = "A quote must either have a bike selected or have parts added.";
 const BIKE_PRICES_REQUIRED = "Bike prices are required to calculate the quote price";
@@ -7,3 +7,35 @@ const CUSTOMER_REQUIRED = "A customer should be selected";
 const QUOTE_PART_ERRORS = "Parts need amending before quote can be saved.";
 const MULTIPLE_REPLACEMENT_PARTS = "Multiple replacement parts - only one allowed per bike part";
 
+export const quotePartValidation = (quotePart, bikePart, partType, brands, parts) => {
+    let validatedQuotePart = updateObject(quotePart);
+    const error_detail = {};
+
+    // reset price if there is no part or this is not a replacement
+
+    if (quotePart.not_required) {
+        validatedQuotePart.quantity = '';
+        if (!partType.can_be_substituted) validatedQuotePart.part_desc = '';
+    }
+
+    if (validatedQuotePart.part_desc) {
+        const part = findPartWithDescription(quotePart.part_desc, partType.id, parts, brands);
+        if (!part) error_detail['part_desc'] = 'Please include a brand in the part name to add this part.';
+        validatedQuotePart.part = part;
+    } else {
+        validatedQuotePart.part = undefined;
+    }
+
+    if (validatedQuotePart.part) {
+        if (!validatedQuotePart.quote_price) error_detail['quote_price'] = 'Please specify a price (can be negative).';
+        if (!quotePart.not_required && !validatedQuotePart.quantity) error_detail['quantity'] = 'Quantity is required for non replacement parts.';
+    } else {
+        if (quotePart.not_required && !validatedQuotePart.quote_price) error_detail['quote_price'] = 'Please specify a price (can be negative).';
+        validatedQuotePart.quantity = '';
+        validatedQuotePart.additional_data = '';
+    }
+
+    validatedQuotePart.error_detail = error_detail;
+    validatedQuotePart.error = isItAnObject(error_detail);
+    return validatedQuotePart;
+};
