f469f7faa0a24b25aefbe4655d5b5e0d12cc9760
Anna <appdev.epiccycles@gmail.com>
2019-05-02 14:25:06 +0100

    remove old react project

--- epic-client/src/components/user/tests/PasswordChange.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PasswordChange from "../PasswordChange";
-
-test("displays correctly", () => {
-    const component = shallow(<PasswordChange
-        changePassword={jest.fn()}
-    />)
-    expect(component).toMatchSnapshot();
-})
