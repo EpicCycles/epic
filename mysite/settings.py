77be5a4c2249d558013b3bb5d0f7b7b66211c3df
Anna <appdev.epiccycles@gmail.com>
2019-05-30 16:28:14 +0100

    remove security vulnerabilities

--- mysite/settings.py
+++ mysite/settings.py
@@ -176,6 +176,23 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 OLD_PASSWORD_FIELD_ENABLED = True
 LOGOUT_ON_PASSWORD_CHANGE = False
 
