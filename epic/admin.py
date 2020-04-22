9e900d0200a350b4d487a33faebd4e84ec1d2faf
Anna Weaver <anna.weaver@johnlewis.co.uk>
2020-04-22 16:40:00 +0100

    Add Brands and suppliers to Admins site

---
+++ epic/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from epic.models.brand_models import Brand
+from epic.models.brand_models import Supplier
+
+admin.site.register(Brand)
+admin.site.register(Supplier)
